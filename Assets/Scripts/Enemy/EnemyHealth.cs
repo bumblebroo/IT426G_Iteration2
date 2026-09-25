d6c64068200737eae13d1bc86c4fd8ddedc8027b
bumblebroo <112719693+bumblebroo@users.noreply.github.com>
2026-09-25 09:49:14 +0200

    organising and enemy hit effect

--- Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
@@ -7,18 +8,38 @@ public class EnemyHealth : MonoBehaviour
 
     private float currentHealth;
 
+    [SerializeField]
+    private Material hitflashMaterial;
+
+    private Material currentMaterial;
+
+    [SerializeField]
+    private float flashDuration;
+
+    [SerializeField]
+    private SpriteRenderer sr;
+
     public float StartHealth => startHealth;
     public float CurrentHealth => currentHealth;
 
     private void Start() {
         currentHealth = startHealth;
+        currentMaterial = sr.material;
     }
 
     public void TakeDamage(float damage) {
         currentHealth -= damage;
 
+        StartCoroutine(flash());
+
         if(currentHealth <= 0) {
             Destroy(this.gameObject);
         }
+    }
+
+    private IEnumerator flash() {
+        sr.material = hitflashMaterial;
+        yield return new WaitForSeconds(flashDuration);
+        sr.material = currentMaterial;
     }
 }
